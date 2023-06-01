128ebf75f3d3950fe6db31b7871c5c8a20b815e3
xyluo <36498464+Xiangyongluo@users.noreply.github.com>
2023-06-01 14:38:13 -0700

    v0.2.2

--- setup.py
+++ setup.py
@@ -10,20 +10,20 @@ import setuptools
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
-    
+
 try:
     # if have requirements.txt file inside the folder
     with open("requirements.txt", "r", encoding="utf-8") as f:
-        modules_needed = [i.strip() for i in fh.readlines()]   
+        modules_needed = [i.strip() for i in fh.readlines()]
 except:
     modules_needed = []
 
 setuptools.setup(
     name="exceltomysql", # Replace with your own username
-    version="0.1.2",
+    version="0.2.2",
     author="Xiangyong Luo",
     author_email="rochemay@163.com",
-    description="This package help to convert your excel files (xlsx,xls,csv) to MySQL database.",
+    description="This package help convert your excel files (xlsx,xls,csv) to MySQL Database.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/Xiangyongluo/exceltomysql",
@@ -35,9 +35,9 @@ setuptools.setup(
     ],
     python_requires='>=3.6',
     install_requires=modules_needed,
-    
+
     packages=setuptools.find_packages(),
     include_package_data=True,
-    package_data= {'':['*.txt','*.xls','*.xlsx','*.csv'],
-                   "test_data":['*.xls']}
+    package_data={'': ['*.txt', '*.xls', '*.xlsx', '*.csv'],
+                  "test_data": ['*.xls']}
 )
