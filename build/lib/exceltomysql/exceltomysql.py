bc60bf8bbe0ba3eac9f4acf8ff07e5f7aa2589a6
xyluo <36498464+Xiangyongluo@users.noreply.github.com>
2023-06-01 14:42:31 -0700

    v0.2.2

--- build/lib/exceltomysql/exceltomysql.py
+++ build/lib/exceltomysql/exceltomysql.py
@@ -6,49 +6,51 @@
 ##############################################################
 
 
+import socket
+
 import pandas as pd
 import pymysql
 import pyodbc
 from sqlalchemy import create_engine
 
-import socket
-
 hostname = socket.gethostname()
 local_ip = socket.gethostbyname(hostname)
 
 
-# Pandas to sql need to use sqlalcemy to create engine
-
-
 class exceltoDBtable:
     #  Available for sql server and mysql now
-    def __init__(self,filePath,server=False,usrID =False,pwd=False,database=False,save2tableName=False):
-        if not any([server,database,usrID,pwd]):
+    def __init__(self,
+                 filePath: str,
+                 server: str = "",
+                 usrID: str = "",
+                 pwd: str = "",
+                 database: str = "",
+                 rename_table: str = ""):
+
+        if not any([server, database, usrID, pwd]):
             raise Exception("Partially inputs, please check your inputs...")
-        else:
-            self.filePath = filePath
-            self.server = server
-            self.database=database
-            self.usrID = usrID
-            self.pwd = pwd
-            self.save2tableName = save2tableName
-        self.dbType = ["mysql","sqlserver"]
+
+        self.filePath = filePath
+        self.server = server
+        self.database = database
+        self.usrID = usrID
+        self.pwd = pwd
+        self.rename_table = rename_table
         self.readData()
         self.connect2DB()
         self.save2database()
 
-    def connect2DB(self) -> "Connect to Database Server":
-    
+    def connect2DB(self) -> None:
+
         try:
             # self.conn = pymysql.connect(host=self.server,user=self.usrID,password=self.pwd,db=self.database,charset="utf8",cursorclass=pymysql.cursors.DictCursor)
-            
-            self.engine = create_engine('mysql+pymysql://%s:%s@%s:3306/%s'%(self.usrID,self.pwd,self.server,self.database))
+            self.engine = create_engine('mysql+pymysql://%s:%s@%s:3306/%s' % (self.usrID, self.pwd, self.server, self.database))
             print("Successfully connected to MySQL...")
-        except:
-            raise Exception("Can not connect to %s, please check your input info."%self.dbType)
+        except Exception:
+            raise Exception("Can not connect to MySQL, please check your input info.")
 
-    def readData(self) -> "DataFrame":
-        if self.filePath.split(".")[-1] in ["xlsx","xls"]:
+    def readData(self) -> None:
+        if self.filePath.split(".")[-1] in ["xlsx", "xls"]:
             self.file_data = pd.read_excel(self.filePath)
             print("Successfully load excel data...")
         elif self.filePath.split(".")[-1] in ["csv"]:
@@ -57,19 +59,16 @@ class exceltoDBtable:
         else:
             raise Exception("Unable to load input file...")
 
-    def save2database(self) ->"DataFrame to database":
-        if self.save2tableName:
-            tableName = self.save2tableName
+    def save2database(self) -> None:
+        if self.rename_table:
+            tableName = self.rename_table
+        elif "/" in self.filePath:
+            tableName = self.filePath.split("/")[-1].split(".")[0]
         else:
-            if "/" in self.filePath:
-                tableName = self.filePath.split("/")[-1].split(".")[0]
-            else:
-                tableName = self.filePath.split(".")[0]
+            tableName = self.filePath.split(".")[0]
+
         try:
-            self.file_data.to_sql(tableName,con=self.engine)
-            print("Successfully save %s into database..."%tableName)
+            self.file_data.to_sql(tableName, con=self.engine)
+            print("Successfully save %s into database..." % tableName)
         except Exception as e:
             raise Exception(e)
-
-
-    
